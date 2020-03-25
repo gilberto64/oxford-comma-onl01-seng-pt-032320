d4f06371a4612b547a9dac995ac671e8b2cde884
Gilberto Ocampo <ocampo@gate.net>
2020-03-25 13:02:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,12 @@
 def oxford_comma(array)
   if array.count == 1 
-  array.join
-  els
-  array.join (" and ")
+    array.join
+  elsif array.count == 2 
+    array.join (" and ")
+  elfif array.count == 3
+    "3"
+  else
+    "more than 3"
+  end
 end
 
