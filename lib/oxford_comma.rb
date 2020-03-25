2340b20e1a5106247cc96fc3513ad22217f38974
Gilberto Ocampo <ocampo@gate.net>
2020-03-25 13:15:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     array.join
   elsif array.count == 2 
     array.join (" and ")
-  elsif array.count == 3
+  elsif array.count > 2
     array.join (", ")
   else
     "more than 3"
